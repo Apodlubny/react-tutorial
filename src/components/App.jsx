7ffb2098a697bad3ad2adce7500e51d53d701ea4
Apodlubny <apodlubny@gmail.com>
2022-11-24 21:57:00 +0200

    box pattern

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,20 @@ import {
   HiBriefcase,
   HiFingerPrint,
 } from 'react-icons/hi';
+import { Box } from '../Box';
 
 export const App = () => {
   return (
     <>
       <RecipeList recipes={recipes} />
-      <Button icon={HiArchive}>Search</Button>
-      <Button icon={HiArrowCircleUp}>Filter</Button>
-      <Button icon={HiBriefcase}>Show Modal</Button>
-      <Button icon={HiFingerPrint} type="submit">
-        Submit
-      </Button>
+      <Box as="section" mt={6}>
+        <Button icon={HiArchive}>Search</Button>
+        <Button icon={HiArrowCircleUp}>Filter</Button>
+        <Button icon={HiBriefcase}>Show Modal</Button>
+        <Button icon={HiFingerPrint} type="submit">
+          Submit
+        </Button>
+      </Box>
     </>
   );
 };
