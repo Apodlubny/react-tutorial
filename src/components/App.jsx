d65869e5276e03b42dac39d3e84c003cb2bb23db
Apodlubny <apodlubny@gmail.com>
2022-11-26 20:38:21 +0200

    formik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { LoginForm } from './FormExample/LoginForm';
+import { ProductReviewForm } from './FormExample/ProductReviewForm';
 import Form from './Forms';
 
 export const App = () => {
@@ -7,6 +9,9 @@ export const App = () => {
   return (
     <div>
       <Form onSubmit={formSubmitHandler} />
+      <LoginForm />
+      <br />
+      <ProductReviewForm />
     </div>
   );
 };
